bd7fba725a19c7fb71b11c3b454243505657367d
leeduckgo <leeduckgo@gmail.com>
2024-01-29 12:09:00 +0800

    update with embedding

--- lib/pureai/openai_embedding.ex
+++ lib/pureai/openai_embedding.ex
@@ -1,35 +1,56 @@
 defmodule Pureai.OpenaiEmbedding do
   use HTTPoison.Base
 
+  def ai_type() do
+    Application.get_env(:openai, :api_type)
+  end
+
+  def api_key() do
+    Application.get_env(:openai, :api_key)
+  end
+
   def client() do
     Tesla.client([
       {Tesla.Middleware.BaseUrl, Application.get_env(:openai, :api_url)},
       Tesla.Middleware.JSON,
-      {Tesla.Middleware.Headers, [{"api-key", Application.get_env(:openai, :api_key)}]}
+      {Tesla.Middleware.Headers, [{"Authorization", "Bearer #{api_key()}"}]}
     ])
   end
 
   def text_to_vetor(prompt) do
     client() |> text_to_vetor(prompt)
   end
+
   def text_to_vetor(client, prompt) do
     sha = hash_text(prompt)
 
     case PureAI.Context.get_embedding_vector_by_sha(sha) do
       nil ->
-        mp = %{:input => prompt}
-        {:ok, %Tesla.Env{status: status, body: %{"data" => [%{"embedding" => vectors}]}}} = Tesla.post(client, "/deployments/text-embedding-ada-002/embeddings?api-version=2023-06-01-preview", mp)
+        ai_type = ai_type()
+        mp = %{:input => prompt, :model => "text-embedding-ada-002"}
+        {:ok, %Tesla.Env{status: status, body: %{"data" => [%{"embedding" => vectors}]}}} = do_text_to_vector(client, mp, :openai)
+        Tesla.post(client, "/v1/embeddings", mp)
 
         case status do
           200 ->
             PureAI.Context.create_embedding_vector(%{sha: sha, text: prompt, vector: Jason.encode!(vectors)})
+
           _ ->
             {:error, status}
         end
 
-      %{vector: _} = res->
+      %{vector: _} = res ->
         {:ok, res}
     end
+  end
+
+  def do_text_to_vector(client, mp, :openai) do
+    {:ok, %Tesla.Env{status: status, body: %{"data" => [%{"embedding" => vectors}]}}} =
+      Tesla.post(
+        client,
+        "/v1/embeddings",
+        mp
+      )
   end
 
   def hash_text(text) do
